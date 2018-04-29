4e09215a7f56f7ef146ffb41b12fa83dfa916a20
globewalldesk <yo.larrysanger@gmail.com>
2018-04-28 22:18:26 -0400

    Added portfolio index & started Portfolio controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,7 @@ require 'faker'
 
 10.times do |blog|
   Blog.create!(
-          title: "#{Faker::MostInterestingManInTheWorld.quote} #{blog}",
+          title: "#{Faker::MostInterestingManInTheWorld.quote}",
           body: Faker::Lorem.paragraph
   )
 end
@@ -11,7 +11,7 @@ puts '10 blog posts created'
 
 5.times do |skill|
   Skill.create!(
-           title: "#{Faker::ProgrammingLanguage.name} #{skill}",
+           title: "#{Faker::ProgrammingLanguage.name}",
            percent_utilized: 15
   )
 end
@@ -20,12 +20,14 @@ puts '5 skills created'
 
 9.times do |portfolio_item|
   Portfolio.create!(
-               title: "#{Faker::Internet.domain_name}  #{portfolio_item}",
+               title: "#{Faker::Internet.domain_name}",
                subtitle: Faker::Job.key_skill,
-               body: Faker::Lorem.sentence(2),
+               body: Faker::Lorem.sentences(3).join(" "),
                main_image: 'http://via.placeholder.com/600x400',
                thumb_image: 'http://via.placeholder.com/350x200'
   )
 end
 
 puts '9 portfolios created'
+
+#### NEXT (4/28) GENERATE SSH KEYS IF NECESSARY...USE ON GITHUB
