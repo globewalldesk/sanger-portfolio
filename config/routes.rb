06e404ad04648015fbbb375ebba40c4fd4e2e7b3
globewalldesk <yo.larrysanger@gmail.com>
2018-05-05 23:43:01 -0400

    changed some routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root 'pages#home'
-  resources :portfolios
-  resources :blogs
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
+  resources :blogs
+  resources :portfolios, except: [:show, :edit]
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
+  get 'portfolio/:id/edit', to: 'portfolios#edit', as: 'edit_portfolio'
 end
