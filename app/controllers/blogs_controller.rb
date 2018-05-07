10c593277d216fc274d78cd528ed4f57845d104e
globewalldesk <yo.larrysanger@gmail.com>
2018-05-06 22:09:34 -0400

    add draft/published status to blogs

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,5 +1,6 @@
 class BlogsController < ApplicationController
-  before_action :set_blog, only: [:show, :edit, :update, :destroy]
+  before_action :set_blog, only: [:show, :edit, :update, :destroy,
+                                  :toggle_status]
 
   # GET /blogs
   # GET /blogs.json
@@ -54,6 +55,13 @@ class BlogsController < ApplicationController
     respond_to do |format|
       format.html { redirect_to blogs_url, notice: 'Blog was successfully destroyed.' }
     end
+  end
+
+  def toggle_status
+    @blog.draft? ?
+        @blog.published! :
+        @blog.draft!
+    redirect_to blogs_url, notice: 'Post status has been updated.'
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,12 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
-  resources :blogs
   resources :portfolios, except: [:show, :edit]
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
   get 'portfolio/:id/edit', to: 'portfolios#edit', as: 'edit_portfolio'
+  resources :blogs do
+    member do
+      get :toggle_status
+    end
+  end
 end
