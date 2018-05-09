371acc915f9dc57b0b256bc08bdd39613c5d47a8
globewalldesk <yo.larrysanger@gmail.com>
2018-05-09 10:11:03 -0400

    Added nested attributes to Portfolio model

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,12 +1,17 @@
 class Portfolio < ApplicationRecord
   has_many :technologies
+  accepts_nested_attributes_for :technologies,
+                                reject_if: lambda { |attrs|
+                                  attrs['name'].blank?
+                                }
+
   include Placeholder
   validates_presence_of :title, :body, :main_image, :thumb_image
 
+  # This and the next 'scope' item accomplish the same thing.
   def self.angular
     where(subtitle: "Angular")
   end
-
   scope :ruby_on_rails_portfolio_items, -> { where(subtitle: 'Ruby on Rails') }
 
   after_initialize :set_defaults
