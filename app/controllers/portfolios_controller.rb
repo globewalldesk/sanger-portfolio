9aed9a27619752d6643b800f0110e8021e831090
globewalldesk <yo.larrysanger@gmail.com>
2018-05-09 21:59:55 -0400

    added form config for tech attributes (portfolios)

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -9,11 +9,14 @@ class PortfoliosController < ApplicationController
 
   def new
     @portfolio_item = Portfolio.new
+    3.times { @portfolio_item.technologies.build }
   end
 
   def create
-    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle,
-                                                            :body))
+    @portfolio_item =
+        Portfolio.new(params.require(:portfolio)
+                          .permit(:title, :subtitle,
+                                  :body, technologies_attributes: [:name]))
     respond_to do |format|
       if @portfolio_item.save
         format.html { redirect_to portfolios_path, notice:
