8c38ab6d1d83e712bd1c03b1b69fdcf958b9aee8
globewalldesk <yo.larrysanger@gmail.com>
2018-05-25 21:53:18 -0400

    Completed authentication with virtual attrs for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates_presence_of :name
+
+  def first_name
+    self.name.split.length > 1 ?
+        self.name.split[0..-2].join(' ') :
+        self.name
+  end
+
+  def last_name
+    self.name.split.last
+  end
 end
